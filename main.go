340784566d448a7da2a94ff0c01251d224c0f68d
Hell1on <artem.bondarchuk1233@gmail.com>
2025-03-06 15:18:11 +0200

    Commit №1 added error handler for timeHandler

--- main.go
+++ main.go
@@ -22,5 +22,7 @@ type TimeResponse struct {
 func timeHandler(w http.ResponseWriter, r *http.Request) {
   response := TimeResponse{Time: time.Now().Format(time.RFC3339)}
   w.Header().Set("Content-Type", "application/json")
-  json.NewEncoder(w).Encode(response)
+  err := json.NewEncoder(w).Encode(response)
+  if err != nil {
+    return
 }
